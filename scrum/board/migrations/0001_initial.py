b1bad2144fadd1b0cae68b73ce63b28776618a16
Chris Kottmyer <Silber8806@gmail.com>
2016-06-11 13:57:54 -0400

    add all

--- scrum/board/migrations/0001_initial.py
+++ scrum/board/migrations/0001_initial.py
@@ -2,21 +2,44 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Sprint',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(default=b'', max_length=100, blank=True)),
-                ('description', models.TextField(default=b'', blank=True)),
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=100, default='', blank=True)),
+                ('description', models.TextField(default='', blank=True)),
                 ('end', models.DateField(unique=True)),
             ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.TextField(default='', blank=True)),
+                ('status', models.SmallIntegerField(default=1, choices=[(1, 'Not Started'), (2, 'In Progress'), (3, 'Testing'), (4, 'Done')])),
+                ('order', models.SmallIntegerField(default=0)),
+                ('started', models.DateField(blank=True, null=True)),
+                ('due', models.DateField(blank=True, null=True)),
+                ('completed', models.DateField(blank=True, null=True)),
+                ('assigned', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, null=True)),
+                ('sprint', models.ForeignKey(to='board.Sprint', blank=True, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
     ]
--- scrum/board/urls.py
+++ scrum/board/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .import views
+from . import views
 
-router = DefaultRouter()
+
+router = DefaultRouter(trailing_slash=False)
 router.register(r'sprints', views.SprintViewSet)
 router.register(r'tasks', views.TaskViewSet)
 router.register(r'users', views.UserViewSet)
-
